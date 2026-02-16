d0ff687fc8e079b58a7a9a644126a7304b2d7fc9
KP <prasanth.25k@gmail.com>
2026-02-16 16:23:56 +0530

    read user input and throw error

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let input = input.trim();
+    println!("{}: command not found", input);
 }
